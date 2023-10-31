d51749f36bf92117746246555faee0eb9a710774
Mosto <prikol.mosto@gmail.com>
2023-10-31 17:21:15 +0700

    Step four. Add Eslint.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
-#!/usr/bin/env node
-import {askname} from "../src/cli.js";
-console.log("Welcome to the Brain Games!");
+#!/usr/bin/env node;
+import { askname } from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 askname();
